daf44bee80a8451d3c9abac5d63948875b5337fa
MagnaDG <produkcja.magna@gmail.com>
2025-08-27 07:09:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("current-date").textContent = new Date().toLocaleDateString("pl-PL");
+
+document.getElementById("save-button").addEventListener("click", () => {
+  alert("Funkcja zapisu zostanie dodana później.");
+});
