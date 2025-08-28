804e4788bcd9772a3d8cb33d107ada8d1904f199
MagnaDG <produkcja.magna@gmail.com>
2025-08-28 07:15:36 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+// wszystkie linie, kody i Tt NOM w jednym miejscu
+const reportData = {
+  "MP4": {
+    "MP4-101": 12,
+    "MP4-102": 15,
+    "MP4-103": 18
+  },
+  "M4": {
+    "M4-201": 10,
+    "M4-202": 20
+  },
+  "NHL": {
+    "NHL-301": 8,
+    "NHL-302": 14,
+    "NHL-303": 16
+  }
+};
